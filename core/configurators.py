cbbd98f906c5d50286bc848163693f7a5629c099
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-06-27 21:35:06 +0800

    feat: 添加快速执行指令yudo

---
+++ core/configurators.py
@@ -0,0 +1,15 @@
+class Command:
+    name: str
+    main: str
+    note: str
+    before: list[str]
+    actions: dict[str, str]
+
+    def __init__(self, *argv, delimiter=' && '):
+        if len(argv) == 1 and argv[0] in self.actions:
+            script = f'{self.main} {self.actions[argv[0]]}'
+        else:
+            script = ' '.join([self.main, *argv])
+
+        self.scripts = [*self.before, script]
+        self.script = delimiter.join([*self.before, script])
---
+++ yudo.py
@@ -0,0 +1,117 @@
+#!./venv/Scripts/python.exe
+# -*- coding: UTF-8 -*-
+"""
+Usage: yudo.py [help|-h|--help]
+       yudo.py [list|-l|--list]
+       yudo.py [debug|-d|--debug] <command> <action>
+       yudo.py [debug|-d|--debug] <command> [<option> ...]
+
+  运行预设的指令（程序／项目／脚本）。
+
+Arguments:
+  command       指令名称。
+  action        指令的快速动作。不存在时会传递给指令本身。
+  option        直接传递给指令的参数。
+  debug         同 --debug
+  list          同 --list
+  help          同 --help
+
+Options:
+  -l, --list    列出所有预设的目标及相应的配置。
+  -d, --debug   解析配置并形成脚本，但不执行，而是直接输出。
+  -h, --help    列出这份帮助信息。
+"""
+import os
+
+import click
+from rich import box
+from rich.table import Table, Column
+
+from core.configurators import Command
+from core.console import HydroConsole
+
+try:
+    import configs
+except ImportError:
+    HydroConsole(stderr=True).warning(
+        '使用此命令前需要配置 ./configs/__init__.py'
+    )
+    exit(-1)
+
+# 让 shulker 检测到
+interface = click.Command(__name__, short_help='运行预设的指令（程序／项目／脚本）')
+
+# 配置里的所有指令
+commands: dict[str, type[Command]] = {
+    v.name: v for v in configs.__dict__.values()
+    if type(v) is type and issubclass(v, Command)
+}
+
+
+def indexes1(iterable):
+    return range(1, len(iterable) + 1)
+
+
+def show_help():
+    print(__doc__)
+    exit(0)
+
+
+def list_commands():
+    table = Table('指令', '描述／备注', box=box.SIMPLE_HEAD)
+    for cmd in commands.values():
+        table.add_row(cmd.name, cmd.note)
+
+    console = HydroConsole()
+    console.print(table)
+    exit(0)
+
+
+def execute(name, *options, debug=False):
+    monitor = HydroConsole(stderr=True)
+    console = HydroConsole()
+    if name is None:
+        monitor.warning('未调用任何指令')
+        exit(-1)
+    if name not in commands:
+        monitor.warning('未找到指令', name)
+        exit(-1)
+
+    command = commands[name](*options)
+
+    if debug:
+        table = Table(
+            Column(justify='right'),
+            Column(),
+            Column(),
+            box=None,
+            padding=(0, 0, 0, 1),
+        )
+        for idx, line in zip(indexes1(command.scripts), command.scripts):
+            table.add_row(str(idx), '│', line)
+        console.print(table)
+        exit(0)
+    try:
+        os.system(command.script)
+    except KeyboardInterrupt:
+        pass
+    exit(0)
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        show_help()
+
+    myself, exe, *argv = sys.argv
+
+    if exe in ('-h', '--help', 'help'):
+        show_help()
+    elif exe in ('-l', '--list', 'list'):
+        list_commands()
+    elif exe in ('-d', '--debug', 'debug'):
+        exe, *argv = argv if argv else [None]
+        execute(exe, *argv, debug=True)
+    else:
+        execute(exe, *argv, debug=False)
